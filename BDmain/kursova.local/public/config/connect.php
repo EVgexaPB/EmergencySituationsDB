f4b083f3d5400ca7fa9ead8f061a73eb2db90467
EVgexaPB <97803160+EVgexaPB@users.noreply.github.com>
2025-04-09 17:12:15 +0300

    Delete BDmain/kursova.local/public directory

--- BDmain/kursova.local/public/config/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connect = mysqli_connect("MySQL-8.2", "root", "", "EmergencySituations");
-
-if (!$connect) {
-    die("Ошибка подключения к базе данных: " . mysqli_connect_error());
-}
--- BDmain/kursova.local/public/vendor/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
- require_once __DIR__ . '/../config/connect.php';
- $victim_id = $_GET['id'];
-
- mysqli_query($connect, "DELETE FROM Victims WHERE `Victims`.`victimID` = '$victim_id'");
- header('Location: /');
